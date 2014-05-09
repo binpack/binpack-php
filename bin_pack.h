196f50ceb90c3cd8ad98726f787ac3d3519e3075
liaohuqiu <liaohuqiu@gmail.com>
2014-05-09 08:55:56 +0800

    for c89 style

--- bin_pack.h
+++ bin_pack.h
@@ -28,22 +28,22 @@ extern "C" {
 #endif
 
 typedef enum {
-	BIN_TYPE_CLOSURE	            = 0x01,	
-	BIN_TYPE_LIST   	            = 0x02,
-	BIN_TYPE_DICT   	            = 0x03,
-	BIN_TYPE_BOOL   	            = 0x04,     /* 0000 0100 T */
+    BIN_TYPE_CLOSURE	            = 0x01,
+    BIN_TYPE_LIST   	            = 0x02,
+    BIN_TYPE_DICT   	            = 0x03,
+    BIN_TYPE_BOOL   	            = 0x04,     /* 0000 0100 T */
     BIN_TYPE_BOOL_FALSE             = 0x05,     /* 0000 0101 F */
-
-	BIN_TYPE_FLOAT_DOUBLE           = 0x06,     /* 0010 0110   */
+    
+    BIN_TYPE_FLOAT_DOUBLE           = 0x06,     /* 0010 0110   */
     BIN_TYPE_FLOAT_SINGLE           = 0x07,     /* 0000 0111   */
-
-	BIN_TYPE_NULL   	            = 0x0f,
-
-	BIN_TYPE_BLOB   	            = 0x10,		/* 0001 xxxx   */
-	BIN_TYPE_STRING   	            = 0x20,		/* 0010 xxxx   */
-
-	BIN_TYPE_INTEGER 	            = 0x40,		/* 010x xxxx + */
-	BIN_TYPE_INTEGER_NEGATIVE    	= 0x60,     /* 011x xxxx - */
+    
+    BIN_TYPE_NULL   	            = 0x0f,
+    
+    BIN_TYPE_BLOB   	            = 0x10,		/* 0001 xxxx   */
+    BIN_TYPE_STRING   	            = 0x20,		/* 0010 xxxx   */
+    
+    BIN_TYPE_INTEGER 	            = 0x40,		/* 010x xxxx + */
+    BIN_TYPE_INTEGER_NEGATIVE    	= 0x60,     /* 011x xxxx - */
 } bin_type_t;
 
 #define BIN_INTEGER_TYPE_64                 0x00 << 3   /* default implementation */
